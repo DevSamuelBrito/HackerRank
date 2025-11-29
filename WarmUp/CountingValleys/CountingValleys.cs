36ee40e33669d4d8f8dbca2f32b397da38f23a0c
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-11-28 23:31:20 -0300

    feat: iadd CountingValleys with C#

---
+++ WarmUp/CountingValleys/CountingValleys.cs
@@ -0,0 +1,70 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        
+        //[UDDDUDUU]
+        int nivel  = 0;
+        int vale = 0;
+        
+        for (var i = 0; i < steps; i++){    
+            if(path[i] == 'U')
+            {
+                nivel++;
+            }else{
+                nivel--;
+            }
+            
+            if (nivel == 0 && path[i] == 'U'){
+                vale++;
+            }
+        }
+        return vale;
+        
+        
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
